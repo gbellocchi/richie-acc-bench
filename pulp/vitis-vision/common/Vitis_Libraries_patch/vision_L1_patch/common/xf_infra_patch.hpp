909de3a6273828748b370b614c2812090e8eba72
Gianluca Bellocchi <gbellocchi95@unimore.it>
2023-03-08 14:33:11 +0100

    Added patches to vitis vision library components under vision/L1/include. I am currently using the Vitis Library from commit @7bf6c52 (from PYNQ Composable Overlay, committed around 2021). Its IPs do not support some features as the new ones from more recent commits (e.g. controlling the FIFO depth from C++ templates). Another workaround that has been accomplished is about the implementation of pipelines that have been modified from the standard design style (stalled, stp) to flushing (flp). The latter prevents deadlocks and improve performance, but costs more resources. It has been essential to run the design inside the RICHIE overlay.

---
+++ pulp/vitis-vision/common/Vitis_Libraries_patch/vision_L1_patch/common/xf_infra_patch.hpp
@@ -0,0 +1,161 @@
+/*
+ * Copyright 2019 Xilinx, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _XF_INFRA_PATCH_H_
+#define _XF_INFRA_PATCH_H_
+
+#ifndef __cplusplus
+#error C++ is needed to use this file!
+#endif
+
+namespace xf {
+namespace cv {
+
+template <int W, int TYPE, int ROWS, int COLS, int NPPC, int XFCVDEPTH>
+int AXIvideo2xfMat_patch(hls::stream<ap_axiu<W, 1, 1, 1> >& AXI_video_strm,
+                   xf::cv::Mat<TYPE, ROWS, COLS, NPPC, XFCVDEPTH>& img) {
+    ap_axiu<W, 1, 1, 1> axi;
+    int res = 0;
+
+    const int m_pix_width = XF_PIXELWIDTH(TYPE, NPPC) * XF_NPIXPERCYCLE(NPPC);
+
+    int rows = img.rows;
+    int cols = img.cols >> XF_BITSHIFT(NPPC);
+    int idx = 0;
+
+    assert(img.rows <= ROWS);
+    assert(img.cols <= COLS);
+
+    bool start = false;
+    bool last = false;
+
+loop_start_hunt:
+    while (!start) {
+// clang-format off
+#pragma HLS pipeline II=1
+#pragma HLS loop_tripcount avg=1 max=1
+        // clang-format on
+
+        AXI_video_strm >> axi;
+        start = axi.user.to_bool();
+    }
+
+loop_row_axi2mat:
+    for (int i = 0; i < rows; i++) {
+        last = false;
+
+    loop_col_zxi2mat:
+        for (int j = 0; j < cols; j++) {
+// clang-format off
+#pragma HLS loop_flatten off
+#pragma HLS pipeline II=1 style=flp
+            // clang-format on
+
+            if (start || last) {
+                start = false;
+            } else {
+                AXI_video_strm >> axi;
+
+                bool user = axi.user.to_int();
+                if (user) {
+                    res |= ERROR_IO_SOF_EARLY;
+                }
+            }
+            if (last && (j != img.cols - 1)) { // checking end of each row
+                res |= ERROR_IO_EOL_EARLY;
+            }
+
+            last = axi.last.to_bool();
+
+            img.write(idx++, axi.data(m_pix_width - 1, 0));
+        }
+
+    loop_last_hunt:
+        while (!last) {
+// clang-format off
+#pragma HLS pipeline II=1
+#pragma HLS loop_tripcount avg=1 max=1
+            // clang-format on
+
+            AXI_video_strm >> axi;
+            last = axi.last.to_bool();
+            res |= ERROR_IO_EOL_LATE;
+        }
+    }
+
+    return res;
+}
+
+// Pack the data of a xf::cv::Mat<> object into an AXI Video stream
+/*
+ *  input: img
+ *  output: AXI_video_strm
+ */
+template <int W, int TYPE, int ROWS, int COLS, int NPPC, int XFCVDEPTH>
+int xfMat2AXIvideo_patch(xf::cv::Mat<TYPE, ROWS, COLS, NPPC, XFCVDEPTH>& img,
+                   hls::stream<ap_axiu<W, 1, 1, 1> >& AXI_video_strm) {
+    ap_axiu<W, 1, 1, 1> axi;
+    int res = 0;
+
+    int rows = img.rows;
+    int cols = img.cols >> XF_BITSHIFT(NPPC);
+    int idx = 0;
+
+    assert(img.rows <= ROWS);
+    assert(img.cols <= COLS);
+
+    const int m_pix_width = XF_PIXELWIDTH(TYPE, NPPC) * XF_NPIXPERCYCLE(NPPC);
+
+    bool sof = true; // Indicates start of frame
+
+loop_row_mat2axi:
+    for (int i = 0; i < rows; i++) {
+    loop_col_mat2axi:
+        for (int j = 0; j < cols; j++) {
+// clang-format off
+#pragma HLS loop_flatten off
+#pragma HLS pipeline II=1 style=flp
+            // clang-format on
+
+            if (sof) {
+                axi.user = 1;
+            } else {
+                axi.user = 0;
+            }
+
+            if (j == cols - 1) {
+                axi.last = 1;
+            } else {
+                axi.last = 0;
+            }
+
+            axi.data = 0;
+            axi.data(m_pix_width - 1, 0) = img.read(idx++);
+
+            axi.keep = -1;
+            AXI_video_strm << axi;
+
+            sof = false;
+        }
+    }
+
+    return res;
+}
+
+} // namespace cv
+} // namespace xf
+
+#endif
