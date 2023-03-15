1755b1510edc94ebc64be43ba52a5545213aba4f
Gianluca Bellocchi <gbellocchi95@unimore.it>
2023-03-15 18:37:35 +0100

    Added new image formats to launch some benchmarking.

---
+++ pulp/vitis-vision/colordetect/sw/src/1920x1080/6cl-16l1p-6l2p-pipeline-const-buffer/color_detect.h
@@ -0,0 +1,233 @@
+/* =====================================================================
+ * Project:      Color detection
+ * Title:        color_detect.c
+ * Description:  Application-level accelerator functions.
+ *
+ * $Date:        8.2.2023
+ * ===================================================================== */
+/*
+ * Copyright (C) 2023 University of Modena and Reggio Emilia.
+ *
+ * Author: Gianluca Bellocchi, University of Modena and Reggio Emilia.
+ *
+ */
+
+#ifndef __COLOR_DETECT_API_H__
+#define __COLOR_DETECT_API_H__
+
+/* System libraries. */
+
+#include <configs.h>
+
+#include <hero-target.h>
+#include <arov-target.h>
+
+#include <common_structs/def_struct_soc_perf_eval.h>
+
+/* Struct */
+
+typedef struct {
+  uint32_t rows; 
+  uint32_t cols; 
+  hwpe_dataset_params_struct standard;
+} hwpe_color_detect_workload_params;
+
+/* Definitions - Parameters mapping */
+
+void rgb2hsv_cv_pipeline_map_params(
+  rgb2hsv_cv_wrapper_struct *wrapper,
+  hwpe_l1_ptr_struct *l1_in_img,  
+  hwpe_l1_ptr_struct *l1_out_img
+);
+
+void threshold_cv_pipeline_map_params(
+  threshold_cv_wrapper_struct *wrapper,
+  hwpe_l1_ptr_struct *l1_in_img,  
+  hwpe_l1_ptr_struct *l1_out_img
+);
+
+void erode_cv_pipeline_map_params(
+  erode_cv_wrapper_struct *wrapper,
+  hwpe_l1_ptr_struct *l1_in_img,  
+  hwpe_l1_ptr_struct *l1_out_img
+);
+
+void dilate_cv_pipeline_map_params(
+  dilate_cv_wrapper_struct *wrapper,
+  hwpe_l1_ptr_struct *l1_in_img,  
+  hwpe_l1_ptr_struct *l1_out_img
+);
+
+static inline void arov_map_params_color_detect(
+  // Accelerator-rich overlay
+  AROV_DEVICE_PTR arov, 
+  // System parameters
+  const int cluster_id, 
+  const int accelerator_id,
+  // L1 accelerator image buffers
+  DEVICE_PTR_CONST l1_in_img,
+  DEVICE_PTR_CONST l1_out_img,
+  uint32_t dim_img_buffer) 
+{ 
+
+  /* L1 buffers */
+
+  // Input image
+  hwpe_l1_ptr_struct _l1_in_img;
+
+  _l1_in_img.ptr = (DEVICE_PTR_CONST)l1_in_img;
+  _l1_in_img.dim_buffer = dim_img_buffer;
+
+  // Output image
+  hwpe_l1_ptr_struct _l1_out_img;
+
+  _l1_out_img.ptr = (DEVICE_PTR_CONST)l1_out_img;
+  _l1_out_img.dim_buffer = dim_img_buffer;
+
+  /* Decide which hardware accelerator to program */
+
+  if(cluster_id == 0){
+    switch (accelerator_id){
+      case 0: rgb2hsv_cv_pipeline_map_params(&(arov->rgb2hsv_cv_0_0), &_l1_in_img, &_l1_out_img); break;
+      default: printf("Error: No matching case for <arov_map_params_color_detect>\n"); break;
+    }
+  }
+
+  if(cluster_id == 1){
+    switch (accelerator_id){
+      case 1: threshold_cv_pipeline_map_params(&(arov->threshold_cv_1_1), &_l1_in_img, &_l1_out_img); break;
+      default: printf("Error: No matching case for <arov_map_params_color_detect>\n"); break;
+    }
+  }
+
+  if(cluster_id == 2){
+    switch (accelerator_id){
+      case 2: erode_cv_pipeline_map_params(&(arov->erode_cv_2_2), &_l1_in_img, &_l1_out_img); break;
+      default: printf("Error: No matching case for <arov_map_params_color_detect>\n"); break;
+    }
+  }
+
+  if(cluster_id == 3){
+    switch (accelerator_id){
+      case 3: dilate_cv_pipeline_map_params(&(arov->dilate_cv_3_3), &_l1_in_img, &_l1_out_img); break;
+      default: printf("Error: No matching case for <arov_map_params_color_detect>\n"); break;
+    }
+  }
+
+  if(cluster_id == 4){
+    switch (accelerator_id){
+      case 4: dilate_cv_pipeline_map_params(&(arov->dilate_cv_4_4), &_l1_in_img, &_l1_out_img); break;
+      default: printf("Error: No matching case for <arov_map_params_color_detect>\n"); break;
+    }
+  }
+
+  if(cluster_id == 5){
+    switch (accelerator_id){
+      case 5: erode_cv_pipeline_map_params(&(arov->erode_cv_5_5), &_l1_in_img, &_l1_out_img); break;
+      default: printf("Error: No matching case for <arov_map_params_color_detect>\n"); break;
+    }
+  }
+};
+
+static inline void arov_swap_buffers_color_detect(
+  // Accelerator-rich overlay
+  AROV_DEVICE_PTR arov, 
+  // System parameters
+  const int cluster_id, 
+  const int buffer_id,
+  // L1 accelerator image buffers
+  DEVICE_PTR_CONST l1_img[l1_n_buffers]) 
+{ 
+
+  if(buffer_id){ // buffer_id == 1
+
+    // Swap buffer pointers for RGB2HSV_CV
+    if(cluster_id == get_cid(RGB2HSV_CV_0)){
+      arov->rgb2hsv_cv_0_0.img_in.tcdm.ptr = l1_img[1]; 
+      arov->rgb2hsv_cv_0_0.img_out.tcdm.ptr = l1_img[3]; 
+      arov_update_buffer_addr(arov, get_cid(RGB2HSV_CV_0), get_aid(RGB2HSV_CV_0));
+    }
+
+    // Swap buffer pointers for THRESHOLD_CV
+    if(cluster_id == get_cid(THRESHOLD_CV_1)){
+      arov->threshold_cv_1_1.img_in.tcdm.ptr = l1_img[1]; 
+      arov->threshold_cv_1_1.img_out.tcdm.ptr = l1_img[3]; 
+      arov_update_buffer_addr(arov, get_cid(THRESHOLD_CV_1), get_aid(THRESHOLD_CV_1));
+    }
+
+    // Swap buffer pointers for ERODE_CV
+    if(cluster_id == get_cid(ERODE_CV_2)){
+      arov->erode_cv_2_2.img_in.tcdm.ptr = l1_img[1]; 
+      arov->erode_cv_2_2.img_out.tcdm.ptr = l1_img[3]; 
+      arov_update_buffer_addr(arov, get_cid(ERODE_CV_2), get_aid(ERODE_CV_2));
+    }
+
+    // Swap buffer pointers for DILATE_CV
+    if(cluster_id == get_cid(DILATE_CV_3)){
+      arov->dilate_cv_3_3.img_in.tcdm.ptr = l1_img[1]; 
+      arov->dilate_cv_3_3.img_out.tcdm.ptr = l1_img[3]; 
+      arov_update_buffer_addr(arov, get_cid(DILATE_CV_3), get_aid(DILATE_CV_3));
+    }
+    
+    // Swap buffer pointers for DILATE_CV
+    if(cluster_id == get_cid(DILATE_CV_4)){
+      arov->dilate_cv_4_4.img_in.tcdm.ptr = l1_img[1]; 
+      arov->dilate_cv_4_4.img_out.tcdm.ptr = l1_img[3]; 
+      arov_update_buffer_addr(arov, get_cid(DILATE_CV_4), get_aid(DILATE_CV_4));
+    }
+
+    // Swap buffer pointers for DILATE_CV
+    if(cluster_id == get_cid(ERODE_CV_5)){
+      arov->erode_cv_5_5.img_in.tcdm.ptr = l1_img[1]; 
+      arov->erode_cv_5_5.img_out.tcdm.ptr = l1_img[3]; 
+      arov_update_buffer_addr(arov, get_cid(ERODE_CV_5), get_aid(ERODE_CV_5));
+    }
+  
+  } else { // buffer_id == 0
+
+    // Swap buffer pointers for RGB2HSV_CV
+    if(cluster_id == get_cid(RGB2HSV_CV_0)){
+      arov->rgb2hsv_cv_0_0.img_in.tcdm.ptr = l1_img[0]; 
+      arov->rgb2hsv_cv_0_0.img_out.tcdm.ptr = l1_img[2]; 
+      arov_update_buffer_addr(arov, get_cid(RGB2HSV_CV_0), get_aid(RGB2HSV_CV_0));
+    }
+
+    // Swap buffer pointers for THRESHOLD_CV
+    if(cluster_id == get_cid(THRESHOLD_CV_1)){
+      arov->threshold_cv_1_1.img_in.tcdm.ptr = l1_img[0]; 
+      arov->threshold_cv_1_1.img_out.tcdm.ptr = l1_img[2]; 
+      arov_update_buffer_addr(arov, get_cid(THRESHOLD_CV_1), get_aid(THRESHOLD_CV_1));
+    }
+
+    // Swap buffer pointers for ERODE_CV
+    if(cluster_id == get_cid(ERODE_CV_2)){
+      arov->erode_cv_2_2.img_in.tcdm.ptr = l1_img[0]; 
+      arov->erode_cv_2_2.img_out.tcdm.ptr = l1_img[2]; 
+      arov_update_buffer_addr(arov, get_cid(ERODE_CV_2), get_aid(ERODE_CV_2));
+    }
+
+    // Swap buffer pointers for DILATE_CV
+    if(cluster_id == get_cid(DILATE_CV_3)){
+      arov->dilate_cv_3_3.img_in.tcdm.ptr = l1_img[0]; 
+      arov->dilate_cv_3_3.img_out.tcdm.ptr = l1_img[2]; 
+      arov_update_buffer_addr(arov, get_cid(DILATE_CV_3), get_aid(DILATE_CV_3));
+    }
+    
+    // Swap buffer pointers for DILATE_CV
+    if(cluster_id == get_cid(DILATE_CV_4)){
+      arov->dilate_cv_4_4.img_in.tcdm.ptr = l1_img[0]; 
+      arov->dilate_cv_4_4.img_out.tcdm.ptr = l1_img[2]; 
+      arov_update_buffer_addr(arov, get_cid(DILATE_CV_4), get_aid(DILATE_CV_4));
+    }
+
+    // Swap buffer pointers for DILATE_CV
+    if(cluster_id == get_cid(ERODE_CV_5)){
+      arov->erode_cv_5_5.img_in.tcdm.ptr = l1_img[0]; 
+      arov->erode_cv_5_5.img_out.tcdm.ptr = l1_img[2]; 
+      arov_update_buffer_addr(arov, get_cid(ERODE_CV_5), get_aid(ERODE_CV_5));
+    }
+
+  }
+};
+
+#endif
---
+++ pulp/vitis-vision/colordetect/sw/src/512x512/1cl-16l1p-1l2p-pipeline-const-buffer/configs.h
@@ -0,0 +1,161 @@
+/* =====================================================================
+ * Project:      System model
+ * Title:        configs.h
+ * Description:  Color detect DSE configuration.
+ *
+ * $Date:        8.2.2023
+ * ===================================================================== */
+/*
+ * Copyright (C) 2023 University of Modena and Reggio Emilia.
+ *
+ * Author: Gianluca Bellocchi, University of Modena and Reggio Emilia.
+ *
+ */
+
+#ifndef __CONFIGS_H__
+#define __CONFIGS_H__
+
+/* =====================================================================
+ * DSE parameters --> Benchmark
+ * ===================================================================== */
+
+// #define _profile_l1_baseline_
+// #define _profile_l1_pipeline_
+// #define _profile_l2_baseline_
+#define _profile_l2_pipeline_single_cl_
+// #define _profile_l2_pipeline_multi_cl_
+
+#define _implement_const_single_buffer_
+// #define _implement_variable_multi_buffer_
+
+/* =====================================================================
+ * DSE parameters --> Application
+ * ===================================================================== */
+
+// Macros for retrieving accelerator integration information
+#define get_cid(val)     ((0x000000FF) & (val >> 0))
+#define get_aid(val)     ((0x000000FF) & (val >> 8))
+#define get_type(val)    ((0x000000FF) & (val >> 16))
+
+#define codify_cid(val) (val << 0)
+#define codify_aid(val) (val << 8)
+#define codify_type(val) (val << 16)
+
+#define my_acc(cid, aid, type) (codify_cid(cid) + codify_aid(aid) + codify_type(type))
+
+// Accelerator integration information
+#define RGB2HSV_CV_0      my_acc(0, 0, RGB2HSV_CV)
+#define THRESHOLD_CV_1    my_acc(0, 1, THRESHOLD_CV)
+#define ERODE_CV_2        my_acc(0, 2, ERODE_CV)
+#define DILATE_CV_3       my_acc(0, 3, DILATE_CV)
+#define DILATE_CV_4       my_acc(0, 4, DILATE_CV)
+#define ERODE_CV_5        my_acc(0, 5, ERODE_CV)
+
+/* =====================================================================
+ * DSE parameters --> System
+ * ===================================================================== */
+
+// System architecture
+#define n_clusters                          2 // 1 accelerator-rich + 1 to mimic bi-directional DMA
+
+// Accelerator-rich
+#define n_acc_total                         6
+#define n_acc_active                        n_acc_total
+#define n_acc_stages                        6 // Total number of processing stages
+
+// Application
+#define n_img                               4 // Number of input images to be processed
+#define img_rows                            512 
+#define img_cols                            512 
+#define img_dim                             img_rows * img_cols
+
+/* =====================================================================
+ * DSE parameters --> L1
+ * ===================================================================== */
+
+// - L1
+#define l1_size_B                           128*1024 // bytes, real dimension
+#define l1_size_B_emulated                  256*1024 // bytes, emulated dimension (>=l1_size_B, must be a power of 2)
+#define n_l1_ports                          16
+#define l1_bank_stride                      1 // to emulate small port utilization (can also be done in HW reducing the number of ports)
+
+// - L1 number of buffers (This depends on the type of optimization being adopted, e.g. pipelining, double buffering)
+#define l1_n_buffers                        4
+
+// L1 stored image tile
+#define l1_img_rows                         128 // do not change, designed on L1=256kB (emulated)
+#define l1_img_cols                         128 // do not change, designed on L1=256kB (emulated)
+#define l1_img_tile                         ((int) (l1_size_B) / ((sizeof(uint32_t)) * (l1_n_buffers))) // designed on L1=128kB (real)
+#define l1_n_buffer_reps                    ((int) ((l1_img_rows) * (l1_img_cols)) / (l1_img_tile))  // to cope with difference between emulated and real L1 memory
+
+// - L1 image buffer
+#define l1_buffer_dim                       ((int) (l1_size_B) / (4 * (l1_n_buffers))) // Dimension of allocated buffer
+                                    // This is also the dimension of the data tile executed by each processing stage of the cluster
+                                    // Data are read row-by-row, so they can sequentialized with 1D DMA transfers
+
+/* =====================================================================
+ * DSE parameters --> L2
+ * ===================================================================== */
+
+// - L2
+#define l2_size_B                           4*1024*1024 // bytes
+#define n_l2_ports_phy                      1
+#define n_l2_ports_virt                     1 // <= n_l2_ports_phy
+#define l2_cl_port_id_offset                0 // Offset on L2 port starting from port 0 (optional, default: 0)
+
+// - L2 multi-port
+#define l2_n_cl_per_port                    ((int) (n_clusters) / (n_l2_ports_virt))
+#define l2_n_bytes_per_port                 ((int) (l2_size_B) / (n_l2_ports_phy))
+#define l2_n_words_per_port                 ((int) (l2_n_bytes_per_port) / (sizeof(uint32_t)))
+
+// - L2 number of buffers (This depends on the type of optimization being adopted, e.g. pipelining, double buffering)
+#define l2_n_buffers                        4
+
+// - L2 image buffer
+#define l2_buffer_dim                       l1_img_tile // Equals dimension of tile passed to L1 memory
+#define l2_n_tiles                          ((int) (img_dim) / ((l1_img_tile) * (l1_n_buffer_reps))) // Number of tiles that are passed to L1 memory
+
+/* =====================================================================
+ * DSE parameters --> Cluster peripherals
+ * ===================================================================== */
+
+// - DMA 
+#define dma_payload_dim                     l1_img_tile // Payload dimension
+#define dma_n_tx                            l1_n_buffer_reps // Number of transfers, designed on L1=128kB (real)
+
+// Event unit
+#define max_num_sw_evt                      8
+
+/* =====================================================================
+ * DSE parameters --> Runtime execution
+ * ===================================================================== */
+
+// - Length of job queue (cache warm-up)
+#define exp_len_job_queue                   2
+
+/* =====================================================================
+ * Additional configurations
+ * ===================================================================== */
+
+/* Performance counters */
+
+#define _allocate_use_perf_event_cycle_
+#define _allocate_perf_event_instr_retired_
+// #define _allocate_perf_event_stall_load_
+// #define _allocate_perf_event_load_
+// #define _allocate_perf_event_store_
+// #define _allocate_perf_event_load_external_
+// #define _allocate_perf_event_store_external_
+
+/* PULP SDK custom configurations */
+
+// #define _pulp_rt_
+#define _pulp_bare_
+
+/* Other macros */
+
+// #define PRINT_LOG
+// #define DEBUG_SYNCH_EU
+// #define INPUT_INIT
+
+#endif
